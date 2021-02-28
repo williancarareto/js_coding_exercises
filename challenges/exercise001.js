dff467d8d023b3b54e3bcbffc99bbf2d35d74fed
willian <williancarareto@gmail.com>
2021-02-28 23:21:48 +0000

    exercise001 capitalize commited

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,6 +1,7 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   // Add your code here!
+  return word.substring(0, 1).toUpperCase() + word.substring(1, word.length);
 }
 
 function generateInitials(firstName, lastName) {
