fbfe9e9423b01b2da097374fb3177d38da459761
willian <williancarareto@gmail.com>
2021-03-02 10:48:01 +0000

    Exercise001 - counting users

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -47,9 +47,15 @@ function reverseAllWords(words) {
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  // Add your code here!
+  // Add your code here! 
  
-}
+    var count = 0;
+    for (var i = 0; i < users.length; i++) 
+      if (users[i].type ==="Linux")  {
+      count++;    
+    }  return count;
+} 
+
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
